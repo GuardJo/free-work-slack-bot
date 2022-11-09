6be1dcc0b9f04b848ca6c2d16d2872c1efccba10
kyeongho <dagh1218@naver.com>
2022-11-09 23:46:26 +0900

    free-work-slack-bot #5 mongodb 관련 설정 추가
    - mongodb 관련 설정 추가
    - 테스트를 위한 embedded mongo 관련 설정 추가

--- free-work-slack-bot/src/test/java/com/guardjo/freeworkslackbot/FreeWorkSlackBotApplicationTests.java
+++ free-work-slack-bot/src/test/java/com/guardjo/freeworkslackbot/FreeWorkSlackBotApplicationTests.java
@@ -1,9 +1,12 @@
 package com.guardjo.freeworkslackbot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FreeWorkSlackBotApplicationTests {
 
     @Test
